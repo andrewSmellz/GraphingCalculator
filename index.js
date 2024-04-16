603cd2583d8fbcd6c9902f59e680daf96871cf52
andrewSmellz <andrewnaquino@gmail.com>
2024-04-15 20:24:19 -0400

    kinda works for making lines but they dont go away help

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const graph = document.getElementById("graph");
+let inputElement = document.getElementById("enterFunction");
+let enteredValue;
 
-
-function graph(){
-
+function clearCanvas() {
+    let graphtx = graph.getContext("2d");
+    graphtx.clearRect(0, 0, 1000, 700);
+    console.log("hello there welcome to it");
 }
 
-document.getElementById("btn").addEventListener("click", graph);
+function graphFunction() {
+    let inputElement = document.getElementById("enterFunction");
+    enteredValue = parseFloat(inputElement.value);
+    clearCanvas();
+    let graphtx = graph.getContext("2d");
+    let centerX = graph.width / 2;
+    let centerY = graph.height / 2;
+    graphtx.moveTo(centerX, centerY);
+    graphtx.lineTo(enteredValue, 100);
+    graphtx.stroke();
+}
+
+document.getElementById("btn").addEventListener("click", graphFunction);
